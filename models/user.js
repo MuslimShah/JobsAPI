703b5c6e741c29d49a5555655582d63444e7f745
Muslim shah <smuslimshah7@gmail.com>
2023-05-23 17:26:19 +0500

    validating email

--- models/user.js
+++ models/user.js
@@ -3,14 +3,22 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'please enter a valid name'],
+        required: [true, 'please provide name'],
         minlength: 3,
         maxlength: 50
     },
     email: {
         type: String,
-        required: [true, 'please enter a valid email']
+        required: [true, 'please provide email'],
+        match: [/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, 'please enter a valid email'],
+        unique: true
 
+    },
+    password: {
+        type: String,
+        required: [true, 'please provide password'],
+        minlength: 6,
+        maxlength: 30
     }
 
 });
