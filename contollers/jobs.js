8ceb20f75f2a0aa8817847048bc9d9143450f58a
Muslim shah <smuslimshah7@gmail.com>
2023-05-24 23:31:15 +0500

    refactor update job

--- contollers/jobs.js
+++ contollers/jobs.js
@@ -1,6 +1,6 @@
 const Jobs = require('../models/jobs');
 const { StatusCodes } = require('http-status-codes');
-const { resouceNotFound } = require('../errors')
+const { resouceNotFound, BadRequest } = require('../errors')
 
 //get all jobs
 exports.getAllJobs = async(req, res) => {
@@ -30,25 +30,39 @@ exports.createJob = async(req, res) => {
     //data from req body
     req.body.createdBy = req.user.id;
     const job = await Jobs.create(req.body);
-    res.status(StatusCodes.CREATED).json({ job });
+    res.status(StatusCodes.CREATED).json({ job, msg: "job created successfully" });
 }
 
 //update job
 exports.updateJob = async(req, res) => {
+    //extracting userid and jobid from the request object
+    const {
+        user: { id: userId },
+        params: { id: jobId },
+        body: { company, position }
+
+    } = req;
+
+    if (company === '' || position === '') {
+        throw new BadRequest('provide valid company or positon')
+    }
+    const job = await Jobs.findOneAndUpdate({ _id: jobId, createdBy: userId }, req.body, { runValidators: true, new: true });
+    if (!job) {
+        throw new resouceNotFound(`job not found with id ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({ job, msg: 'job updated successfully' })
+
+}
+
+//delete job
+exports.deleteJob = async(req, res) => {
     //extracting userid and jobid from the request object
     const { user: { id: userId }, params: { id: jobId } } = req;
     const job = await Jobs.findOne({ _id: jobId, createdBy: userId });
     if (!job) {
         throw new resouceNotFound(`oops! no job found  with id:${jobId} `)
     }
-    job.company = req.body.company;
-    job.position = req.body.position;
-    job.save();
-    res.status(StatusCodes.OK).json({ msg: 'job updated successfully' })
-
-}
-
-//delete job
-exports.deleteJob = async(req, res) => {
-    res.send('delete  job');
+    const deletedJob = await Jobs.deleteOne({ _id: jobId, createdBy: userId });
+    res.status(StatusCodes.OK).json({ msg: `job with id:${jobId} deleted successfully` })
 }
