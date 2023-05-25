2e5549d1f5fa47cc576fe3b368b6a59302acc401
Muslim shah <smuslimshah7@gmail.com>
2023-05-25 18:40:25 +0500

    refactoring code

--- app.js
+++ app.js
@@ -1,3 +1,68 @@
+/*
+ * Project Name: JobsAPI
+ * Description: This API allows users to register, create, update, and delete jobs.
+ * Author: Muslim Shah
+ * Date: May 25, 2023
+ *
+ * Routes:
+ * - /api/v1/auth
+ *   - POST /register
+ *   - POST /login
+ *
+ * - /api/v1/jobs [Authenticate User]
+ *   - GET /        [Get all jobs created by the user]
+ *   - POST /       [Create a new job]
+ *   - GET /:id     [Get a single job by ID]
+ *   - PATCH /:id   [Update a job by ID]
+ *   - DELETE /:id  [Delete a job by ID]
+ *
+ * Responses:
+ * - Register Response:
+ *   - Status: 201
+ *   - Body: { user: { name: user.name }, token }
+ *
+ * - Login Response:
+ *   - Status: 200
+ *   - Body: { user: { name: user.name }, token }
+ *
+ * - Get All Jobs Response:
+ *   - Status: 200
+ *   - Body: { jobs: allJobs, count: allJobs.length }
+ *
+ * - Get Single Job Response:
+ *   - Status: 200
+ *   - Body: singleJob
+ *
+ * - Create Job Response:
+ *   - Status: 201
+ *   - Body: { job, msg: "Job created successfully" }
+ *
+ * - Update Job Response:
+ *   - Status: 200
+ *   - Body: { job, msg: "Job updated successfully" }
+ *
+ * - Delete Job Response:
+ *   - Status: 200
+ *   - Body: { msg: "Job deleted successfully" }
+ *
+ * Security Packages:
+ * - helmet: Provides various security-related HTTP headers.
+ * - cors: Enables Cross-Origin Resource Sharing (CORS) for handling requests from different domains.
+ * - xss-clean: Prevents Cross-Site Scripting (XSS) attacks by sanitizing user input.
+ * - express-limiter: Implements rate limiting to protect against brute force and denial-of-service attacks.
+ *
+ * Error Handling:
+ * - express-async-errors: Handles asynchronous errors in Express middleware and routes.
+ *
+ * Environment Variables:
+ * - dotenv: Loads environment variables from a .env file into process.env.
+ *
+ * Database:
+ * - MongoDB: A NoSQL database for storing user and job data.
+ * - Mongoose: An Object Data Modeling (ODM) library for MongoDB to provide a more intuitive interface for interacting with the database.
+ */
+
+
 require('express-async-errors');
 require('dotenv').config();
 const express = require('express');
@@ -19,8 +84,7 @@ const connectDb = require('./database/database');
 //my routes
 const authRoutes = require('./routes/auth');
 const jobsRoutes = require('./routes/jobs');
-
-// app.use(express.urlencoded({ extended: true }));
+//setting up trust proxy
 app.set('trust proxy', 1) //for heruku
 app.use(rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
